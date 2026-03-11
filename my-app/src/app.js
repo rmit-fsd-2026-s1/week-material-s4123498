dbc3d2405468ba01cd050db9b4d3532be4cf3ba1
JackyChang0812 <east.horng27@gmail.com>
2026-03-11 20:11:56 +1100

    initial test commit

---
+++ my-app/src/app.js
@@ -0,0 +1,6 @@
+var userName = "John";
+var age = 25;
+var isStudent = true;
+var hobbies = ["reading", "gaming", "coding"];
+console.log(userName);
+console.log(age);
---
+++ my-app/src/app.ts
@@ -0,0 +1,6 @@
+let userName : string = "John"; let age: number = 25;
+let isStudent: boolean = true;
+let hobbies: string[] = ["reading", "gaming", "coding"];
+
+console.log(userName);
+console.log(age);
